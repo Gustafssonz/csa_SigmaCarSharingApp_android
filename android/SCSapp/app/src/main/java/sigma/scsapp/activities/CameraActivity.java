e318f7ce735d4a100547c3c791f0769e8128043d
LucianaHaugen <lucianahaugen@gmail.com>
2017-10-16 14:17:19 +0200

    Camera Activity - accessing camera

--- android/SCSapp/app/src/main/java/sigma/scsapp/activities/CameraActivity.java
+++ android/SCSapp/app/src/main/java/sigma/scsapp/activities/CameraActivity.java
@@ -1,16 +1,44 @@
 package sigma.scsapp.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import sigma.scsapp.R;
 
 public class CameraActivity extends AppCompatActivity {
 
+    ImageView imageViewCamera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        ImageView imageViewCamera = (ImageView)findViewById(R.id.imageViewCamera);
+
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageViewCamera.setImageBitmap(bitmap);
     }
 }
 
