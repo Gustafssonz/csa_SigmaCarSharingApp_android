b6fc35da645b7b14e612c1eba5bc24c118dbb42c
LucianaHaugen <lucianahaugen@gmail.com>
2017-10-16 14:03:48 +0200

    first Camera Commit: Add camera layout

---
+++ android/SCSapp/app/src/main/java/sigma/scsapp/activities/CameraActivity.java
@@ -0,0 +1,16 @@
+package sigma.scsapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sigma.scsapp.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
+
